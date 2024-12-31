9599c8e3d41dc6fb39fc6390992e4e50bfac3c51
Remanshi Gajera <128599179+remanshi289@users.noreply.github.com>
2024-12-31 17:10:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,7 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+ name_on_order+ ' !', icon="✅")
 
 
+import requests
+smoothiefroot_responses = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
